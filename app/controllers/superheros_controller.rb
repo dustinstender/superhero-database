22368ffd82fccbdf1e2ac7f68b8fd7fa31cb3596
Dustin Stender <dustinpstender@gmail.com>
2022-01-17 18:42:35 +0100

    adding superheros works

--- app/controllers/superheros_controller.rb
+++ app/controllers/superheros_controller.rb
@@ -3,6 +3,24 @@ class SuperherosController < ApplicationController
     @superheros = Superhero.all
   end
 
+  def search
+    @superheros = Superhero.all
+    if params[:superhero].present?
+      @superhero = Superhero.search_hero(params[:superhero])
+      if @superhero
+        render 'superheros/index'
+      else
+        flash[:alert] = 'We could not find this superhero'
+        redirect_to superheros_path
+      end
+    else
+      flash[:alert] = 'You must enter something'
+      redirect_to superheros_path
+    end
+  end
+
   def create
+    @superhero = Superhero.create!(name: params[:name], intelligence: params[:intelligence])
+    redirect_to superheros_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'superhero', to: 'superheros#search'
   resources :superheros, only: [:index, :create]
 end
